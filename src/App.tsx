c383c85263c52bdfa6ce08e14c9cdcbcd06effb7
min <zzagg777@gmail.com>
2026-04-01 17:41:59 +0900

    chore: 라이브러리 설치 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <div>Hello React 👋</div>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind 동작 확인! 🎉
+      </h1>
+    </div>
   );
 }
 
